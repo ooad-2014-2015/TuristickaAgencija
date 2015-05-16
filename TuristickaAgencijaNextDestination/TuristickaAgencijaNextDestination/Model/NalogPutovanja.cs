b703c5a47f0664b3f1b487ac03cc97503d6e5679
Ramiza Hoso <rhoso1@etf.unsa.ba>
2015-05-16 15:26:07 +0200

    verzija2

---
+++ TuristickaAgencijaNextDestination/TuristickaAgencijaNextDestination/Model/NalogPutovanja.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuristickaAgencijaNextDestination.Model
+{
+    class NalogPutovanja : iRacun
+    {
+        public int _id { get; set; }
+
+        public Klijent _klijent { get; set; }
+
+        public decimal _cijena { get; set; }
+
+        public Putovanje _putovanje { get; set; }
+
+        public decimal _popust { get; set; }
+
+        public NalogPutovanja(int id, decimal cijena, decimal popust)
+        {
+            _id = id;
+            _cijena = cijena;
+            _popust = popust;
+        }
+
+        //public decimal obracunajPopust(); 
+
+       // public override decimal generisiRacun()
+
+      //  public string posaljiObavijest();
+
+      //  public Boolean otkaziPutovanje();
+
+    }
+}
