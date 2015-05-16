f002174ce847381bbb1d9a7ad1cba20f351ea28a
lbajgoric1 <lbajgoric1@etf.unsa.ba>
2015-05-16 17:28:56 +0200

    ispostovan strategy pattern

---
+++ Igrica/Hangman/Hangman/Grad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Grad : IOblast
+    {
+        public void Oblast(string oblast)
+        {
+            oblast = "City";
+        }
+    }
+}
---
+++ Igrica/Hangman/Hangman/Vjesala.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    public class Vjesala
+    {
+        private IOblast oblast;
+        private ITezinaIgre tezinaIgre;
+        public Igrac igrac { get; set; }
+        public int brojacNepogodjenihSlova { get; set; }
+
+        public Vjesala()
+        {
+
+        }
+
+        public Vjesala(IOblast _oblast, ITezinaIgre _tezinaIgre)
+        {
+            oblast = _oblast;
+            tezinaIgre = _tezinaIgre;
+            brojacNepogodjenihSlova = 0;
+        }
+
+        public void Oblast(string _oblast)
+        {
+            oblast.Oblast(_oblast);
+        }
+
+        public void TezinaIgre(string _tezina)
+        {
+            tezinaIgre.TezinaIgre(_tezina);
+        }
+    }
+}
