5890c5eeccf0890d56f5ce3ed8d4545749dcd2e9
Merseda Camdzic <mcamdzic1@etf.unsa.ba>
2015-05-19 17:00:56 +0200

    Verzija3

--- TuristickaAgencijaNextDestination/TuristickaAgencijaNextDestination/Model/Klijent.cs
+++ TuristickaAgencijaNextDestination/TuristickaAgencijaNextDestination/Model/Klijent.cs
@@ -7,17 +7,19 @@ using System.Threading.Tasks;
 namespace TuristickaAgencijaNextDestination
 {
     enum StatusKlijenta {student, penzioner, ostalo};
-    class Klijent : Osoba
+ class Klijent : Osoba
     {
         public int brojKreditneKartice { get; set; }
 
         public StatusKlijenta statusKlijenta { get; set; }
-
+      
+      
         public Klijent(int _id, String _ime, String _prezime, String _eMail, int _broj, StatusKlijenta _status) 
             : base(_id, _ime, _prezime, _eMail)
         {
             brojKreditneKartice = _broj;
             statusKlijenta = _status;
         }
+      
     }
 }
