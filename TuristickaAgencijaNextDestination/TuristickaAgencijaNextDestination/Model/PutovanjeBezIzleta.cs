c463366dbd6703103505b642ef91855d0dd78243
Ramiza Hoso <rhoso1@etf.unsa.ba>
2015-05-21 12:03:29 +0200

    Liste

--- TuristickaAgencijaNextDestination/TuristickaAgencijaNextDestination/Model/PutovanjeBezIzleta.cs
+++ TuristickaAgencijaNextDestination/TuristickaAgencijaNextDestination/Model/PutovanjeBezIzleta.cs
@@ -20,6 +20,7 @@ namespace TuristickaAgencijaNextDestination.Model
 
         public PutovanjaBezIzleta() { }
         public static List<Putovanje> listaPutovanjaBezIzleta = new List<Putovanje>();
+
         public static void DodajPutovanjeBezIzleta()
         {
 
@@ -36,6 +37,22 @@ namespace TuristickaAgencijaNextDestination.Model
             listaPutovanjaBezIzleta.Add(putovanje5);*/
 
         }
+        
+        //Ramiza : dodala sam ovaj konstruktor jer mi je tako trebalo 
+
+        public PutovanjaBezIzleta(string destinacija, double cijena, DateTime datumPolaska, DateTime datumDolaska, int trajanjePutovanja, int brojSlobodnihMjesta, PrevoznoSredstvo prevoznoSredstvo, PutnoOsiguranje putnoOsiguranje, int id)
+            : base(destinacija, cijena, datumPolaska, datumDolaska, trajanjePutovanja, brojSlobodnihMjesta, prevoznoSredstvo, putnoOsiguranje, id) { }
+      
+        public static List<Putovanje> listaPredlozenihPutovanjaBezIzleta = new List<Putovanje>();
+       
+       /* public static void DodajPredlozenoPutovanjeBezIzleta()
+        {
+            Putovanje _putovanje1 = new PutovanjaBezIzleta ("Maldivi", 1350.0, DateTime.Now, DateTime.Now, 15, 30, PrevoznoSredstvo.avion, PutnoOsiguranje.ASAOsiguranje, 22);
+            Putovanje _putovanje2 = new PutovanjaBezIzleta ("Pariz", 500.0, DateTime.Now, DateTime.Now, 10, 50, PrevoznoSredstvo.autobus, PutnoOsiguranje.ASAOsiguranje, 33);
+            listaPredlozenihPutovanjaBezIzleta.Add(_putovanje1);
+            listaPredlozenihPutovanjaBezIzleta.Add(_putovanje2);
+        }
+        */
         public static int DajMiNaredniID()
         {
             return listaPutovanjaBezIzleta.Count + 1;
